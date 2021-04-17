e35be87ccb66fbfa2c21114f24883f88603ef527
JulianaAlmeida10 <56660441+JulianaAlmeida10@users.noreply.github.com>
2021-04-16 22:49:34 -0300

    Registrando Chamadas
    É realizado o registro de chamadas em um arquivo.hd, criado pelo back-end do php no lado do servidor, armazenado as informações automaticamente.

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -46,16 +46,16 @@
             <div class="card-body">
               <div class="row">
                 <div class="col">
-                  
-                  <form>
+                  <!-- 'action' -> é definido qual é o destino para qual será feito o submit do formulario, no momento que o botão 'submit' for clicado  -->
+                  <form method="post" action="registra_chamado.php">
                     <div class="form-group">
                       <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+                      <input name="titulo" type="text" class="form-control" placeholder="Título">
                     </div>
                     
                     <div class="form-group">
                       <label>Categoria</label>
-                      <select class="form-control">
+                      <select name="categoria" class="form-control">
                         <option>Criação Usuário</option>
                         <option>Impressora</option>
                         <option>Hardware</option>
@@ -66,7 +66,7 @@
                     
                     <div class="form-group">
                       <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
+                      <textarea name="descricao" class="form-control" rows="3"></textarea>
                     </div>
 
                     <div class="row mt-5">
---
+++ registra_chamado.php
@@ -0,0 +1,42 @@
+<?php
+    #Recuperando as informações dos inputs do 'abrir_chamado.php'
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
+
+    /* TRABALHANDO NA MONTAGEM DO TEXTO**************************
+        a separação da concatenação das string será sepadaras por #, logo tem que fazer um tratamento no back-end, que se um usuario digitar #, no momento da concatenaçã será convertida em '-'
+
+        *Isso evita Bug
+
+        str_replace('parametro existente','será substituido por...', 'onde procurar')
+    *************************************************************/
+    $titulo = str_replace('#','-', $_POST['titulo']);
+    $categoria = str_replace('#','-', $_POST['categoria']);
+    $descricao = str_replace('#','-', $_POST['descricao']);
+
+    //texto que será armazenado dentro do arquivo.hd
+    //OBS: este texto é uma string
+    //PHP_EOL -> essa contante armazena o caracter de quebra de linha conforme o sistema operacional onde o php esta rodando
+    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+
+    #abrir
+    //fopen('nome do arquivo (arquivo.txt)', 'especificar o que vc ira fazer no arquivo (o parametro "a" abre o aquivo para escrita)')
+    //$arquivo esta recebendo a REFERENCIA do arquivo
+    $arquivo = fopen('arquivo.hd','a');
+
+    #escrever
+    //escrevendo o texto no arquivo
+    /* 
+        fwrite('referencia do arquivo que abrimos','(string) o que eu quero escrever , dentro do arquivo')
+    */
+    fwrite($arquivo, $texto);
+
+
+    #fechar
+    fclose($arquivo);
+
+
+    header('Location: abrir_chamado.php');
+?>
